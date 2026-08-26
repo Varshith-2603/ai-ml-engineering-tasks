996aa586ace7bf67bda16e5c8728741270b9d739
THALLA VARSHITH <varshith.thalla@gmail.com>
2026-08-26 08:34:58 +0530

    Created maths

---
+++ Mathematics/Calculus/BASIC DIFFERENTIATION RULES.py
@@ -0,0 +1,16 @@
+x = 2
+function = 3*x**5+2*x**3+7
+first_derivative = 15*x**4+6*x**2
+print("First Derivative:",first_derivative)
+
+
+x = 1
+function = 8*x**6+5*x**3+2*x-10
+first_derivative = 48*x**5+15*x**2+2
+print("First Derivative:",first_derivative)
+
+w = 2
+loss_function = 3*w**2+5
+partial_w = 6*w+5
+first_derivative_2 = 6*w+5
+print("First Derivative w.r.t 2:",first_derivative_2)
